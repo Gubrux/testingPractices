5be6ccc125ffd7e5ab6be5644847eab394cef323
Gubrux <gabrielcentur@gmail.com>
2024-04-04 20:36:09 -0400

    duplicated

--- test/02-template-string.test.js
+++
@@ -1,9 +0,0 @@
-import { getSaludo } from "../src/base-pruebas/02-template-string";
-
-describe("Pruebas en 02 template strung", () => {
-    test("este es un texto", () => {
-        const name = 'Fernando';
-        const message = getSaludo(name);
-        expect(message).toBe(`Hola ${name}`);
-    });
-});
