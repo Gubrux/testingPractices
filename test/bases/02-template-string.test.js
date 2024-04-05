5197087733befdb35e83238361315d675e66585e
Gubrux <gabrielcentur@gmail.com>
2024-04-04 20:34:54 -0400

    finished test in 02

---
+++ test/bases/02-template-string.test.js
@@ -0,0 +1,9 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+describe("Pruebas en 02 template strung", () => {
+    test("este es un texto", () => {
+        const name = 'Fernando';
+        const message = getSaludo(name);
+        expect(message).toBe(`Hola ${name}`);
+    });
+});
